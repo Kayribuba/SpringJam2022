7a7d0ce405d7f47e113fa63b9d6fc785d4531e7f
Kayribuba <70769641+Kayribuba@users.noreply.github.com>
2022-05-14 01:42:01 +0300

    aptal unity simulator
    aptal unity

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants
+{
+    public static string PlayerTag = "Player";
+    public static string EnemyTag = "Enemy";
+    public static string GroundTag = "Ground";
+    public static string UnityArrowXTag = "UnityArrowX";
+    public static string UnityArrowYTag = "UnityArrowY";
+    public static string UnityArrowZTag = "UnityArrowZ";
+    public static string UnityArrowBaseTag = "UnityArrowBase";
+}
---
+++ Assets/Scripts/UnityPowers.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityPowers : MonoBehaviour
+{
+    public LayerMask unityArrowLayer;
+    public GameObject safe;
+    [Range(1, 250)] public float PlatformSpeed = 50f;
+
+    GameObject ArrowParent;
+    Vector3 targetPosition;
+    bool holdingArrows;
+    Camera cam;
+    RaycastHit hit;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        ArrowParent = safe;
+    }
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit TempHit;
+
+        if (Physics.Raycast(ray, out TempHit, 100, unityArrowLayer))
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                hit = TempHit;
+                holdingArrows = true;
+                ArrowParent = hit.transform.parent.gameObject;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            holdingArrows = false;
+        }
+
+        if (holdingArrows)
+        {
+            targetPosition = ArrowParent.transform.position;
+
+            switch (hit.transform.gameObject.tag)
+            {
+                case "UnityArrowX":
+                    targetPosition.x -= Input.GetAxis("Mouse X") * PlatformSpeed / 250;
+                    break;
+
+                case "UnityArrowY":
+                    targetPosition.y += Input.GetAxis("Mouse Y") * PlatformSpeed / 250;
+                    break;
+
+                case "UnityArrowZ":
+                    targetPosition.z -= Input.GetAxis("Mouse X") * PlatformSpeed / 250;
+                    break;
+            }
+
+                ArrowParent.transform.position = targetPosition;
+        }
+    }
+}

63dd7f1403b5bb3f514f944314bdefa12ccedfe7
Kayribuba <70769641+Kayribuba@users.noreply.github.com>
2022-05-15 15:57:06 +0300

    al awq
    heyaheyahey

--- Assets/Scripts/AudioPlayer.cs
+++ Assets/Scripts/AudioPlayer.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class AudioPlayer : MonoBehaviour
 {
     public static AudioPlayer instance;
+    public AudioClip[] clips;
+    public AudioSource source;
+
+    int oldLevel = -1;
 
     void Start()
     {
@@ -19,8 +23,58 @@ public class AudioPlayer : MonoBehaviour
         }
 
     }
-    void Update()
+    void OnLevelWasLoaded(int level)
     {
-        
+        switch(level)
+        {
+            case 0:
+                if (oldLevel != 0)
+                {
+                    source.clip = clips[0];
+                    oldLevel = 0;
+                    source.Play();
+                }
+                break;
+            case 1:
+                if (oldLevel != 1)
+                {
+                    source.clip = clips[1];
+                    oldLevel = 1;
+                    source.Play();
+                }
+                break;
+            case 2:
+                if (oldLevel != 2)
+                {
+                    source.clip = clips[2];
+                    oldLevel = 2;
+                    source.Play();
+                }
+                break;
+            case 3:
+                if (oldLevel != 3)
+                {
+                    source.clip = clips[3];
+                    oldLevel = 3;
+                    source.Play();
+                }
+                break;
+            case 4:
+                if (oldLevel != 4)
+                {
+                    source.clip = clips[4];
+                    oldLevel = 4;
+                    source.Play();
+                }
+                break;
+            case 5:
+                if (oldLevel != 5)
+                {
+                    source.clip = clips[5];
+                    oldLevel = 5;
+                    source.PlayOneShot(clips[5]);
+                }
+                break;
+        }
     }
 }

ec228df7a4ac0c54120239e363d66a2bbef17c7f
Mikee1353 <murat_tunc07@windowslive.com>
2022-06-29 12:59:00 +0300

    ses

--- Assets/Scripts/options.cs
+++ Assets/Scripts/options.cs
@@ -7,12 +7,23 @@ using UnityEngine.Audio;
 public class options : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public AudioSource MusicAudioSource;
     public Slider valuebar;
+    public Slider musicValuebar;
     float valuee;
 
+    private void Start()
+    {
+        musicValuebar.value = MusicAudioSource.volume;
+    }
     public void SetVolume()
     {
         valuee = valuebar.value;
         audioMixer.SetFloat("volume", valuee);
+    }
+    public void SetMusicValue()
+    {
+        valuee = musicValuebar.value;
+        MusicAudioSource.volume = valuee;
     }
 }

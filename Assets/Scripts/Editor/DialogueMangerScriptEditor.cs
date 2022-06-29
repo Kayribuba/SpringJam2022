77761a405dc1bff6acae1cc8612773a5ffad7213
Kayribuba <70769641+Kayribuba@users.noreply.github.com>
2022-06-29 12:01:21 +0300

    312

---
+++ Assets/Scripts/Editor/DialogueMangerScriptEditor.cs
@@ -0,0 +1,16 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEditor;
+
+
+//[CustomEditor(typeof(DialogueMangerScript))]
+//public class DialogueMangerScriptEditor : Editor
+//{
+//    public override void OnInspectorGUI()
+//    {
+//        base.OnInspectorGUI();
+//        DialogueMangerScript dms = (DialogueMangerScript)target;
+//        GUILayout.BeginHorizontal();
+//    }
+//}
